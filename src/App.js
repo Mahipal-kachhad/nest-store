90a06f51d8d22ce47cb4f96c00f8e69a97674c81
Mahipal Kachhad <mahipalkac@gmail.com>
2025-05-27 14:11:28 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./App.css";
 import Home from "./components/home/Home";
@@ -9,7 +9,6 @@ import SingleProduct from "./components/shop/SingleProduct";
 import Pages from "./components/pages/Pages";
 import SimpleSlider from "./components/shop/SimpleSlider";
 import { ToastContainer } from "react-toastify";
-
 
 function App() {
   return (
@@ -22,7 +21,11 @@ function App() {
         <Route path="/pages/*" element={<Pages />} />
         <Route path="/simpleSlider" element={<SimpleSlider />} />
       </Routes>
-        <ToastContainer position="top-right" autoClose={1000} />
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        style={{ marginTop: "70px" }}
+      />
     </div>
   );
 }
